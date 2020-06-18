2a16b0487f84fbd8b9165c7c585b089454b5329b
Christian Bunker <christian@bunkerplanet.com>
2020-06-18 16:36:00 -0400

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,1 +1,12 @@
+'''
+This module is part of the DoublePendulum computational physics project, as implemented by
+Christian Bunker and Leo Bunker, June-July 2020. Credit to Allen B Downey's works "Think
+Python" and "Think Complexity" and Stickler and Schachinger's book "Basic Concepts in
+Computational Physics" for their help.
+
+Written in Python 3.7.3
+
+errors.py implements specific child classes of Exception for use throughout this project
+'''
+
 
