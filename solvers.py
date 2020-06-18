d385af2206be97b823089e14386c22193138548f
Christian Bunker <christian@bunkerplanet.com>
2020-06-18 18:30:55 -0400

    Begin writing solvers

---
+++ solvers.py
@@ -0,0 +1,10 @@
+'''
+This module is part of the DoublePendulum computational physics project, as implemented by
+Christian Bunker and Leo Bunker, June-July 2020. Credit to Allen B Downey's works "Think
+Python" and "Think Complexity" and Stickler and Schachinger's book "Basic Concepts in
+Computational Physics" for their help.
+
+Written in Python 3.7.3
+
+solvers.py implements various ways of solving the initial-value ODE, y'(t) = F(y, t)
+'''
