b40ca22b7abcdb9f9cb959022396cc73404585e0
Christian Bunker <christian@bunkerplanet.com>
2020-06-18 20:04:04 -0400

    implement forward rectangular method

--- solvers.py
+++ solvers.py
@@ -8,3 +8,41 @@ Written in Python 3.7.3
 
 solvers.py implements various ways of solving the initial-value ODE, y'(t) = F(y, t)
 '''
+
+import errors
+
+import numpy as np
+
+#######################################################################
+# define ODE solvers
+#######################################################################
+
+def ForwardRectangular(yn, tn, f, dt):
+    '''
+    Forward rectangular rule or explicit Euler method for integration stepping,
+    used to solve problem of form y(t) = \int f(t) dt
+    
+    Args:
+    yn, double, the nth y value of function y which is the result of the integration
+    tn, double, nth value of the variable being integrated over
+    f, function pointer, the function which is the integrand
+    dt, double, the step of the integration variable
+    
+    Returns:
+    double, the y_{n+1}th value of y
+    '''
+    
+    return yn + f(yn, tn)*dt; #### end ForwardRectangular
+
+
+#######################################################################
+# test code
+#######################################################################
+
+
+#######################################################################
+# execute code
+#######################################################################
+if(__name__ == "__main__"):
+
+    pass;
